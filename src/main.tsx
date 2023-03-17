666a089825d576ebab203ee022958e10a48b0216
Madalena Machado Rocha <rochamada1997m@gmail.com>
2023-03-17 15:28:18 -0300

    :tada: initial commit

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+// TypeScript: Ferramenta de tipagem estática para o JavaScript
+// Tipagem estática: Mecanismo para evitar erros enquanto estamos desenvolvendo o app
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+
+  </React.StrictMode>,
+)
